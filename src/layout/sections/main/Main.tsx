9ecf50e1cf9373a1ede988b8120daefa86ed847d
Сергей <privet15.89@mail.ru>
2023-09-01 17:44:23 +0500

    feat: main (part)

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 import mainPhoto from 'assets/images/mainPhoto.webp'
 import {FlexContainer} from "components/FlexContainer";
 import styled from "styled-components";
+import {Container} from "components/Container";
+import {theme} from "styles/Theme";
 
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexContainer align={'center'} justify={'space-around'}>
-                <div>
-                    <span>Hello!</span>
-                    <Name>I’m Baden Baden</Name>
-                    <span>I’am freelance</span> <MainTitle>web developer.</MainTitle>
-                </div>
+            <Container>
+                <FlexContainer align={'center'} justify={'space-between'}>
+                    <div>
+                        <span>Hello!</span>
+                        <Name>I’m Sergey</Name>
+                        <span>I build things for web</span>
+                    </div>
 
-                <Photo src={mainPhoto} alt=""/>
-            </FlexContainer>
+                    <Photo></Photo>
+                </FlexContainer>
+            </Container>
         </StyledMain>
     );
 };
@@ -22,18 +26,32 @@ export const Main = () => {
 
 const StyledMain = styled.section`
   min-height: 100vh;
+  display: flex;
+
+  color: ${theme.colors.fontMainTitle};
+  font-size: 50px;
+  font-weight: 700;
+  line-height: 70px;
+  letter-spacing: -1px;
 `
 
-const Photo = styled.img`
+const Photo = styled.div`
+  position: relative;
   width: 350px;
   height: 350px;
-  object-fit: cover;
-`
-
-const MainTitle = styled.h1`
-
+  border-radius: 50%;
+  border: 9px solid;
+  border-image: ${theme.colors.gradientBg} .5;
+  background: url(${mainPhoto}) lightgray 50% / cover no-repeat;
 `
 
 const Name = styled.h2`
-
+  background: ${theme.colors.gradientBg};
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 58px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 70px;
+  letter-spacing: -1px;
 `
