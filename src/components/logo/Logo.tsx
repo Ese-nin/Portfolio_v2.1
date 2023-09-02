06354d3bbabd46f6b26462f563aa07fa176881f7
Сергей <privet15.89@mail.ru>
2023-09-02 23:19:04 +0500

    ? gradient icon

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import styled from "styled-components";
 import {Icon} from "../icon/Icon";
 
 export const Logo = () => {
     return (
-        <a href="#">
-            <Icon iconId={'github'}/>
-        </a>
+        <StyledDiv>
+            <Link href="#">
+                <Icon iconId='logo' viewBox="0 0 70 70"/>
+            </Link>
+        </StyledDiv>
     );
 };
+
+const StyledDiv = styled.div`
+  color: #fff;
+  display: inline-block;
+  height: 100px;
+  width: 100px;
+  background-color: linear-gradient(90deg, #13B0F5 -2.06%, #E70FAA 100%);
+  -webkit-background-clip: text;
+`
+
+const Link = styled.a`
+
+`
