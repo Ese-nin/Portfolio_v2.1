4c3c910b69309a1ec6df10b1ce9f366a61f711ba
Сергей <privet15.89@mail.ru>
2023-09-04 00:13:53 +0500

    logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -13,12 +13,9 @@ export const Logo = () => {
 };
 
 const StyledDiv = styled.div`
-  color: #fff;
-  display: inline-block;
   height: 100px;
   width: 100px;
   background-color: linear-gradient(90deg, #13B0F5 -2.06%, #E70FAA 100%);
-  -webkit-background-clip: text;
 `
 
 const Link = styled.a`
