646e2e3840ab8bc47fa122e886f5e4fea807f307
Сергей <privet15.89@mail.ru>
2023-09-03 16:54:03 +0500

    slogan/contacts

--- src/layout/sections/contactMe/Slogan.tsx
+++ src/layout/sections/contactMe/Slogan.tsx
@@ -1,27 +1,40 @@
+import {Container} from "components/Container";
+import {FlexContainer} from "components/FlexContainer";
 import React from 'react';
 import styled from "styled-components";
 
 export const Slogan = () => {
     return (
         <StyledSlogan>
-            <Title>For any questions please mail me:</Title>
-            <Email>email@mail.com</Email>
+            <Container>
+                <FlexContainer direction="column" align="center">
+                    <Title>For any questions please mail me:</Title>
+                    <Email>email@mail.com</Email>
+                </FlexContainer>
+            </Container>
         </StyledSlogan>
     );
 };
 
 
 const StyledSlogan = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  min-height: 30vh;
+
 `
 
 const Title = styled.h2`
-  
+  font-family: DM Sans, sans-serif;
+  font-size: 58px;
+  font-weight: 700;
+  letter-spacing: -1px;
 `
 
 const Email = styled.span`
+  font-family: DM Sans, sans-serif;
+  font-size: 58px;
+  font-weight: 700;
+  letter-spacing: -1px;
 
+  background: linear-gradient(90deg, #13B0F5 -2.06%, #E70FAA 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 `
