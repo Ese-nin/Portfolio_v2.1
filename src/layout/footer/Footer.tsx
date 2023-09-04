9daf0874969f527acce46fd3610cab2dc97a6b2f
Сергей <privet15.89@mail.ru>
2023-09-04 22:40:29 +0500

    fix: width

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,3 +1,4 @@
+import {Container} from "components/Container";
 import {Icon} from "components/icon/Icon";
 import {Menu} from "components/menu/Menu";
 import React from 'react';
@@ -7,22 +8,24 @@ import {theme} from "styles/Theme";
 export const Footer = () => {
     return (
         <StyledFooter>
-            <TopBlock>
-                <Icon iconId='logo' height="70" width="70" viewBox='0 0 70 70'/>
-                <Contacts>
-                    <span>+1234567890</span>
-                    <span>qwerty@qwerty.com</span>
-                    <div>
-                        <a href="#">
-                            <Icon iconId='git_contact' height='30' width='30' viewBox='0 0 40 40'/>
-                        </a>
-                        <a href="#">
-                            <Icon iconId='telegram' height='30' width='30' viewBox='0 0 45 45'/>
-                        </a>
-                    </div>
-                </Contacts>
-            </TopBlock>
-            <Menu/>
+            <Container>
+                <TopBlock>
+                    <Icon iconId='logo' height="70" width="70" viewBox='0 0 70 70'/>
+                    <Contacts>
+                        <span>+1234567890</span>
+                        <span>qwerty@qwerty.com</span>
+                        <div>
+                            <a href="#">
+                                <Icon iconId='git_contact' height='30' width='30' viewBox='0 0 40 40'/>
+                            </a>
+                            <a href="#">
+                                <Icon iconId='telegram' height='30' width='30' viewBox='0 0 45 45'/>
+                            </a>
+                        </div>
+                    </Contacts>
+                </TopBlock>
+                <Menu/>
+            </Container>
         </StyledFooter>
     );
 };
@@ -38,14 +41,14 @@ const TopBlock = styled.div`
   height: 50%;
   margin-bottom: 50px;
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
 
   &::after {
     content: "";
     position: absolute;
     display: inline-block;
     height: 5px;
-    width: 80%;
+    width: 100%;
     bottom: -20px;
 
     background-color: rgba(66, 68, 110, 0.3);
