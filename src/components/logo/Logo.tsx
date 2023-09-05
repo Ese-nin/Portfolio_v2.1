92f0682f54de0968ce553366d1954120ea7c36a9
Сергей <privet15.89@mail.ru>
2023-09-05 18:52:18 +0500

    delete useless code

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,23 +1,10 @@
+import logo from "assets/images/logo.svg"
 import React from 'react';
-import styled from "styled-components";
-import {Icon} from "../icon/Icon";
 
 export const Logo = () => {
     return (
-        <StyledDiv>
-            <Link href="#">
-                <Icon iconId='logo' viewBox="0 0 70 70"/>
-            </Link>
-        </StyledDiv>
+            <a href="#">
+                <img src={logo} alt="logo"/>
+            </a>
     );
-};
-
-const StyledDiv = styled.div`
-  height: 100px;
-  width: 100px;
-  background-color: linear-gradient(90deg, #13B0F5 -2.06%, #E70FAA 100%);
-`
-
-const Link = styled.a`
-
-`
+}
